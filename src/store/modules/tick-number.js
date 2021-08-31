c8a42b29ea6493cb6fbed1496e4bbe48cf53bd54
Colin Zang <zyy0116@gmail.com>
2021-08-31 22:49:01 +0800

    Update tick-number.js

--- src/store/modules/tick-number.js
+++ src/store/modules/tick-number.js
@@ -1,15 +1,31 @@
 import Vue from "vue";
 
+const LOCALMAX = 16;
+const BARMAX = 8;
+
 const state = {
+    globalTick: -1,
+    localTick: -1,
+    barTick: -1
 }
 
 const getters = {
+    getGlobalTick (state) {
+        return state.globalTick;
+    }
 }
 
 const actions = {
 }
 
 const mutations = {
+    addTick (state) {
+        state.globalTick += 1;
+        state.localTick += 1;
+        state.barTick += 1;
+        state.localTick = state.localTick % LOCALMAX;
+        state.barTick = state.barTick % BARMAX;
+    }
 }
 
 export default {
