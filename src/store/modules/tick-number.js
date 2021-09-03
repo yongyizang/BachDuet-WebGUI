47462d51470352b9721b4c126ef833a02813d638
GHD0424 <63272713+GHD0424@users.noreply.github.com>
2021-09-03 10:24:13 -0400

    Update tick-number.js

--- src/store/modules/tick-number.js
+++ src/store/modules/tick-number.js
@@ -1,14 +1,21 @@
 
 import Vue from "vue";
 
+//Maximum of the localTick
 const LOCALMAX = 16;
 
+/*Initial state of three Tick number
+globalTick is the Tick number for the whole process of the midi IO
+localTick is the Tick number for only one measure, cannot be greater than 16
+barTick is the number of the measures played
+ */
 const state = {
     globalTick: -1,
     localTick: -1,
     barTick: -1
 }
 
+//Getters for globalTick, localTick, barTick
 const getters = {
     getGlobalTick (state) {
         return state.globalTick;
@@ -24,9 +31,14 @@ const getters = {
 const actions = {
 }
 
+/*
+AddTick is the func. for changing the value of the three Tick number.
+ */
 const mutations = {
     addTick (state) {
         state.globalTick += 1;
+        state.localTick += 1;
+        state.barTick += 1;
         state.localTick = state.globalTick % 16;
         state.barTick = state.globalTick / 16;
     }
