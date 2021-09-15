7aba172ba07c8a84dc2c54c78b84cc405ae7fa20
popDesktop <xribene55@gmail.com>
2021-09-15 08:45:29 -0400

    clock-metronome mute behavior/deleted midi playback

--- src/views/main.vue
+++ src/views/main.vue
@@ -30,14 +30,14 @@
       </button>
     </div>
     <div class="timingControls">
-      <button class="octs" @click="togglePlayback">
+      <!-- <button class="octs" @click="togglePlayback">
         {{ playbackMessage }}
-      </button>
+      </button> -->
       <span style="color: white"
-        >BPM:<input id="bpm" v-model.lazy="BPM"
+        >BPM:<input id="bpm" v-model.lazy="BPM" maxlength="3" size="3" 
       /></span>
       <span style="color: white"
-        >Freq:<input id="freq" v-model.lazy="FREQ"
+        >Freq:<input id="freq" v-model.lazy="FREQ" maxlength="2" size="3"
       /></span>
       <button class="octs" @click="toggleClock">Clock</button>
     </div>
@@ -61,7 +61,7 @@ const userMap = [
 ];
 
 // Initialize Piano Sampler 1. This is for User.
-// Q: where is the User piano sampler 1 ? ???
+// C: where is the User piano sampler 1 ? ???
 
 // Initialize is done within the UI Component. See components/keyboardUI.vue
 // For every user in userMap, we create a sampler, which sends to a separate bus.
@@ -83,7 +83,7 @@ const metronomeSampler = new Instruments().createSampler(
 );
 const metronomeBus = new Tone.Channel().toDestination();
 metronomeSampler.connect(metronomeBus);
-//Q: how about user and ai bus ? 
+//C: how about user and ai bus ? 
 
 // Metronome Behavior.
 // Decouple later.
@@ -116,6 +116,7 @@ metronomeSampler.connect(metronomeBus);
 //   }
 // }
 
+// C: what does this do ? 
 window.onclick = () => {
   Tone.start();
   Tone.context.lookAhead = 0;
@@ -132,18 +133,18 @@ export default {
       FREQ: 4,
       bpm: 60,
       intervalIntegar: 4,
-      tickNumber: -1,
+      tickNumber: -1, // C: TODO remove that, and use VUEX
       clockStatus: false,
       clockInitialized: false,
-      clockNumber: -1,
+      // clockNumber: -1,
       screenWidth: document.body.clientWidth,
       screenHeight: document.body.clientHeight,
       keyboardUIKey: 0,
       keyboardUIoctaveStart: 1,
       keyboardUIoctaveEnd: 6,
       metronomeStatus: true,
-      playbackMessage: "Start THE Playback",
-      playing: false
+      // playbackMessage: "Start THE Playback",
+      // playing: false
     };
   },
 
@@ -215,28 +216,29 @@ export default {
       // Right now it only updates the message.
       // Easy to customize into doing other stuff in the future.
       if (this.metronomeStatus) {
-        this.metronomeMessage = "Metronome On";
+        // this.metronomeMessage = "Metronome On";
         metronomeBus.mute = true;
         console.log("Metronome is muted.");
       } else {
-        this.metronomeMessage = "Metronome Mute";
+        // this.metronomeMessage = "Metronome Mute";
         metronomeBus.mute = false;
         console.log("Metronome is On.");
       }
       this.metronomeStatus = !this.metronomeStatus;
     },
 
-    togglePlayback() {
-      Tone.start();
-      if (this.playing) {
-        Transport.pause();
-        this.playbackMessage = "Start THE Playback";
-      } else {
-        Transport.start();
-        this.playbackMessage = "Pause THE Playback";
-      }
-      this.playing = !this.playing;
-    },
+    // C: not needed
+    // togglePlayback() {
+    //   Tone.start();
+    //   if (this.playing) {
+    //     Transport.pause();
+    //     this.playbackMessage = "Start THE Playback";
+    //   } else {
+    //     Transport.start();
+    //     this.playbackMessage = "Pause THE Playback";
+    //   }
+    //   this.playing = !this.playing;
+    // },
 
     transposeOctUp() {
       this.keyboardUIoctaveStart += 1;
@@ -253,11 +255,14 @@ export default {
       var vm = this;
       // Allowing tickNumber to add to itself.
       vm.clockStatus = !vm.clockStatus;
-      if (vm.clockStatus) {
-        if (metronomeBus.muted) metronomeBus.mute = false;
-      } else {
-        metronomeBus.mute = true;
-      }
+
+      // C: we don't need this if else statement
+      // if (vm.clockStatus) {
+      //   if (metronomeBus.muted) metronomeBus.mute = false;
+      // } else {
+      //   metronomeBus.mute = true;
+      // }
+
       // If the clock is not yet initialized...
       if (!vm.clockInitialized) {
         // Then set it to intialized
@@ -270,7 +275,7 @@ export default {
         function tickBehavior(){
           if (vm.clockStatus){
               vm.tickNumber += 1;
-          }
+            // }
               // Below are behaviors.
               console.log(
                 "Tick #" +
@@ -282,10 +287,11 @@ export default {
 
               // Reset global BufferState.
               vm.$store.commit("clearBuffer");
+          }
         }
 
         function sendOutTicks() {
-          console.log("tick send.");
+          // console.log("tick send.");
           tickBehavior();
           setTimeout(sendOutTicks, ((60 / vm.bpm / 4) * 1000));
         }
@@ -304,55 +310,56 @@ export default {
   },
 
   created() {
-    Tone.context.lookAhead = 0;
-    const instruments = new Instruments();
+    // C: This is not needed (?)
+    // Tone.context.lookAhead = 0;
+    // const instruments = new Instruments();
 
-    instruments.createSampler("piano", (piano) => {
-      piano.release = 2;
-      piano.toDestination();
+    // instruments.createSampler("piano", (piano) => {
+    //   piano.release = 2;
+    //   piano.toDestination();
 
-      // A bare minimum room reverb.
-      const reverb = new Tone.Reverb({
-        predelay: 0.125,
-        decay: 1.3,
-        wet: 0.5,
-      });
-      piano.chain(reverb, Tone.Destination);
+    //   // A bare minimum room reverb.
+    //   const reverb = new Tone.Reverb({
+    //     predelay: 0.125,
+    //     decay: 1.3,
+    //     wet: 0.5,
+    //   });
+    //   piano.chain(reverb, Tone.Destination);
 
-      const now = Tone.now() + 0.5;
-      Midi.fromUrl("/audio/midi/demo.mid")
-        .then((midi) => {
-          midi.tracks.forEach((track) => {
-            track.notes.forEach((note) => {
-              Transport.schedule(() => {
-                piano.triggerAttackRelease(
-                  note.name,
-                  note.duration,
-                  Tone.now(),
-                  note.velocity
-                );
-              }, note.time + now);
+    //   const now = Tone.now() + 0.5;
+    //   Midi.fromUrl("/audio/midi/demo.mid")
+    //     .then((midi) => {
+    //       midi.tracks.forEach((track) => {
+    //         track.notes.forEach((note) => {
+    //           Transport.schedule(() => {
+    //             piano.triggerAttackRelease(
+    //               note.name,
+    //               note.duration,
+    //               Tone.now(),
+    //               note.velocity
+    //             );
+    //           }, note.time + now);
 
-              Transport.schedule((time) => {
-                Draw.schedule(() => {
-                  pianoState[note.name] = true;
-                }, time);
-              }, note.time + now);
+    //           Transport.schedule((time) => {
+    //             Draw.schedule(() => {
+    //               pianoState[note.name] = true;
+    //             }, time);
+    //           }, note.time + now);
 
-              Transport.schedule((time) => {
-                Draw.schedule(() => {
-                  pianoState[note.name] = false;
-                }, time);
-              }, note.time + note.duration + now);
-            });
-          });
-        })
-        .catch(console.error);
-    });
+    //           Transport.schedule((time) => {
+    //             Draw.schedule(() => {
+    //               pianoState[note.name] = false;
+    //             }, time);
+    //           }, note.time + note.duration + now);
+    //         });
+    //       });
+    //     })
+    //     .catch(console.error);
+    // });
 
-    Buffer.on("error", (error) => { 
-      console.error(error);
-    });
+    // Buffer.on("error", (error) => { 
+    //   console.error(error);
+    // });
   },
 
   computed: {
@@ -361,7 +368,8 @@ export default {
     },
 
     activeNote() {
-      return this.music[this.currentNoteIndex]; // Q: what is this.music
+      // C: what is this.music
+      return this.music[this.currentNoteIndex]; 
     },
   },
 };
