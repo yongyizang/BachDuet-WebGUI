5e25ed3f872212969f2def070e331a2198189257
Colin Zang <zyy0116@gmail.com>
2021-11-06 23:45:14 -0400

    Deleted VueMaterial

--- src/main.js
+++ src/main.js
@@ -3,12 +3,8 @@ import store from './store/index.js'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
-import VueMaterial from 'vue-material'
-import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default.css'
 
 Vue.config.productionTip = false
-Vue.use(VueMaterial)
 
 new Vue({
   router,
