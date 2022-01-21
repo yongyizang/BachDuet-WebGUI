20d9e2435b066a5f1f2cd33a959cf63c5fedb0d5
Colin Zang <zyy0116@gmail.com>
2022-01-20 20:15:49 -0500

    Update MIDI.vue with self-updating dropdown

--- src/components/MIDI.vue
+++ src/components/MIDI.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="MIDIInput">
     <select v-model="selectedMIDIDevice" @change="onMIDIDeviceSelectedChange">
-      <option v-for="item in activeDevices" :value="item.index">{{item.name}}</option>
+      <option v-for="item in activeDevices" :value="item.index">
+        {{ item.name }}
+      </option>
     </select>
   </div>
 </template>
@@ -11,25 +13,28 @@ import { WebMidi } from "webmidi";
 var midiAccess;
 
 export default {
-  created() {
-
-  },
+  created() {},
 
   mounted() {
+    var vm = this;
+    navigator.requestMIDIAccess().then(function (access) {
+      access.onstatechange = vm.onEnabled;
+    });
+
     // Enable WebMIDI, then call onEnabled method.
     WebMidi.enable()
-      .then(this.onEnabled)
-      .catch((err) => alert(err)); 
+      .then(vm.onEnabled)
+      .catch((err) => alert(err));
   },
 
   methods: {
     onEnabled() {
       var vm = this;
       if (WebMidi.inputs.length < 1) {
-        console.log("No device detected.");
+        vm.activeDevices = [];
       } else {
         WebMidi.inputs.forEach((device) => {
-          vm.activeDevices.push({"index": device.id, "name": device.name});
+          vm.activeDevices.push({ index: device.id, name: device.name });
         });
         this.selectedMIDIDevice = this.activeDevices[0].index;
         this.messageListener();
@@ -37,39 +42,38 @@ export default {
     },
 
     onMIDIDeviceSelectedChange() {
-      console.log("MIDI Device Changed to " + this.selectedMIDIDevice)
+      console.log("MIDI Device Changed to " + this.selectedMIDIDevice);
     },
 
-    messageListener(){
+    messageListener() {
       var vm = this;
       const inputDevice = WebMidi.getInputById(this.selectedMIDIDevice);
-      inputDevice.addListener("noteon", message => {
+      inputDevice.addListener("noteon", (message) => {
         vm.$root.$refs.keyboardUI.toggleAttack(message.note.identifier);
       });
-      inputDevice.addListener("noteoff", message => {
+      inputDevice.addListener("noteoff", (message) => {
         vm.$root.$refs.keyboardUI.toggleRelease(message.note.identifier);
       });
-    }
+      inputDevice.addListener("disconnected", vm.onEnabled);
+    },
   },
 
   data() {
     return {
       activeDevices: [],
-      selectedMIDIDevice: '',
+      selectedMIDIDevice: "",
     };
   },
 };
 </script>
 
 <style scoped>
-
 .MIDIInput {
-  z-index:999;
-  position:absolute;
-  top:20;
-  left:20;
-  width:100px;
-  height:50px;
+  z-index: 999;
+  position: absolute;
+  top: 20;
+  left: 20;
+  width: 100px;
+  height: 50px;
 }
-
 </style>
