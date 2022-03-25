73d2c05fb9ccd7a60680f1d403f95f3ed1362f80
Colin Zang <zyy0116@gmail.com>
2022-03-25 10:50:47 -0400

    small bug fixes and interface alignment

--- src/views/main.vue
+++ src/views/main.vue
@@ -34,7 +34,8 @@
           We highly recommend using Chrome for better user experience.
         </p>
         <p v-if="isMobile">
-          The model may not perform normally on mobile devices. We recommend using Desktop computers.
+          The model may not perform normally on mobile devices. We recommend
+          using Desktop computers.
         </p>
       </div>
     </div>
@@ -221,6 +222,7 @@
             <div class="md-layout-item md-small-size-50 md-xsmall-size-100">
               <div class="settingsDiv">
                 <p class="settingsOptionTitle">BPM</p>
+                <div style="padding-top:14px">
                 <p class="settingsValue">{{ BPM }}</p>
                 <vue-slider
                   v-model="BPM"
@@ -229,16 +231,17 @@
                   :max="120"
                   class="settingsSlider"
                 ></vue-slider>
+                </div>
               </div>
             </div>
             <div class="md-layout-item md-small-size-50 md-xsmall-size-100">
-              <div class="settingsDiv" style="padding-top: 10px">
+              <div class="settingsDiv">
                 <span class="settingsOptionTitle">Metronome</span>
                 <toggle-button
                   color="#74601c"
                   :value="true"
                   @change="toggleMetronome"
-                  style="transform: scale(0.9)"
+                  style="transform: scale(0.9); padding-top: 17px"
                 />
               </div>
             </div>
@@ -323,10 +326,11 @@
               class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100"
             >
               <span>
-                By clicking this button, you would destroy all data sent to our
-                server, including all your play data and all app performance
-                data. All your data is gathered anonymously, and would only be
-                used for research purposes.
+                If you chose to send us your data, all your data is gathered anonymously, and would only be
+                used for research purposes.<br />
+                If you want to delete all data sent to our
+                server, click the big red button. It would destroy all your play data and all app performance
+                data.
               </span>
             </div>
             <div
@@ -370,26 +374,30 @@
             Introduction
           </p>
           <p>
-            Hi! This is BachDuet. You could play with our model in real time,
-            which is trained on baroque chorales.
+            Hi! This is BachDuet, your AI partner for collaborative
+            improvisation in the style of Bach chorales. We recommend using
+            <span style="font-weight: 600">Chrome</span> and desktop/laptop
+            computers for best support.
             <br /><br />
-            You could play with computer keyboard, on-screen keyboard or MIDI
-            keyboard. MIDI keyboard support depends on whether your browser
-            supports Web MIDI.
+
+            Simply click on the “Play” button and start playing. You could use
+            your computer keyboard, the on-screen keyboard or an external MIDI
+            keyboard.
             <br /><br />
-            We recommend using <span style="font-weight: 600">Chrome</span> for
-            best support. <br /><br />
-            You could see the real-time interaction being shown in both a
-            musical-game style and a traditional, score-based style. They are
-            both in real-time.
+
+            In “Settings”, you can change the metronome, the volume, the
+            randomness of the AI model, and choose to delete all of your data
+            after playing.
             <br /><br />
-            You could change volume and delete all your data from our server
-            using the settings panel. Hit play to start playing.
+
+            If you encounter many pop up "bug" messages, it might be because the
+            neural network is not running fast enough on your device. You can
+            try decreasing the metronome's BPM or using a more powerful device.
             <br /><br />
-            If you encounter any problem during playing, feel free to leave us
-            some feedback. We would greatly appreciate your input!
+
+            If you encounter any problem, please let us know. We would greatly
+            appreciate your input!
             <br /><br />
-            Click outside this modal to close this modal.
           </p>
         </div>
       </modal>
@@ -477,8 +485,13 @@ export default {
       userNoteBuffer2Firebase: [],
       AINoteBuffer2Firebase: [],
       isNotChrome: navigator.userAgent.indexOf("Chrome") <= -1,
-      isMobile: /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
-    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4)),
+      isMobile:
+        /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(
+          navigator.userAgent
+        ) ||
+        /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+          navigator.userAgent.substr(0, 4)
+        ),
       firebaseErrCount: 0,
       misalignErrCount: 0,
     };
@@ -696,23 +709,23 @@ export default {
     },
     firebaseErrCount: {
       immediate: true,
-      handler(newValue){
-        if (newValue == 10){
+      handler(newValue) {
+        if (newValue == 10) {
           this.$toasted.show(
-              "We cannot collect data from your session. Don't worry, your application will still run smoothly!"
-            );
+            "We cannot collect data from your session. Don't worry, your application will still run smoothly!"
+          );
         }
-      }
+      },
     },
     misalignErrCount: {
       immediate: true,
-      handler(newValue){
-        if (newValue == 10){
+      handler(newValue) {
+        if (newValue == 10) {
           this.$toasted.show(
-              "Your local machine cannot run inference at this speed. Try lowering the BPM."
-            );
+            "Your local machine cannot run inference at this speed. Try lowering the BPM."
+          );
         }
-      }
+      },
     },
     keyboardUIoctaveStart: {
       immediate: true,
@@ -904,10 +917,10 @@ export default {
             vm.userDataID = docRef.id;
             vm.$store.commit("writeSessionID", docRef.id);
           } catch (e) {
-            if (vm.firebaseErrCount < 10){
+            if (vm.firebaseErrCount < 10) {
               this.$toasted.show(
-              "Error adding doc to firebase. Error Message in console."
-            );
+                "Error adding doc to firebase. Error Message in console."
+              );
               console.log("Firebase error:", e);
               vm.firebaseErrCount += 1;
             }
@@ -1124,13 +1137,13 @@ export default {
                 vm.userNoteBuffer2Firebase = [];
                 vm.AINoteBuffer2Firebase = [];
               } catch (e) {
-            if (vm.firebaseErrCount < 10){
-              this.$toasted.show(
-              "Error adding doc to firebase. Error Message in console."
-            );
-              console.log("Firebase error:", e);
-              vm.firebaseErrCount += 1;
-            }
+                if (vm.firebaseErrCount < 10) {
+                  this.$toasted.show(
+                    "Error adding doc to firebase. Error Message in console."
+                  );
+                  console.log("Firebase error:", e);
+                  vm.firebaseErrCount += 1;
+                }
               }
             }
 
@@ -1241,13 +1254,13 @@ export default {
       try {
         await deleteDoc(doc(db, "data", vm.$store.getters.getSessionID));
       } catch (e) {
-            if (vm.firebaseErrCount < 10){
-              this.$toasted.show(
-              "Error adding doc to firebase. Error Message in console."
-            );
-              console.log("Firebase error:", e);
-              vm.firebaseErrCount += 1;
-            }
+        if (vm.firebaseErrCount < 10) {
+          this.$toasted.show(
+            "Error adding doc to firebase. Error Message in console."
+          );
+          console.log("Firebase error:", e);
+          vm.firebaseErrCount += 1;
+        }
       }
       window.location.reload(true);
     },
@@ -1263,13 +1276,13 @@ export default {
           }),
         });
       } catch (e) {
-            if (vm.firebaseErrCount < 10){
-              this.$toasted.show(
-              "Error adding doc to firebase. Error Message in console."
-            );
-              console.log("Firebase error:", e);
-              vm.firebaseErrCount += 1;
-            }
+        if (vm.firebaseErrCount < 10) {
+          this.$toasted.show(
+            "Error adding doc to firebase. Error Message in console."
+          );
+          console.log("Firebase error:", e);
+          vm.firebaseErrCount += 1;
+        }
       }
       vm.feedbackRating = 5.0;
       vm.feedbackText = "";
