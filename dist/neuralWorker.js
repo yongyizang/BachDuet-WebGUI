50de909ee41eff4f3a163d8cad1a0c18ce78e566
Yongyi (Colin) Zang <zyy0116@gmail.com>
2023-05-31 13:36:44 -0400

    Failed online compile

---
+++ dist/neuralWorker.js
@@ -0,0 +1,102 @@
+importScripts("/tf.min.js")
+
+const CHECKPOINT_BASE_URL = "/checkpoints/"
+
+// var fs = require('fs');
+
+async function loadModels(){
+    self.modelLstm = await tf.loadLayersModel('checkpoints/modelsFinal_Lstm/model.json');
+    self.modelEmb =  await tf.loadLayersModel('checkpoints/modelsFinal_Emb/model_cleaned.json');
+    
+    // Post this message to UI
+    // TODO: explain what is service worker and what is post message
+    postMessage("AI 巴赫已经加载好啦！");
+
+    // tf backend doc
+    tf.setBackend('webgl');
+
+    warmupRounds = 2;
+    var midiInp = tf.tensor2d([[96, 96]]);
+    var cpcInp = tf.tensor2d([[12, 12]]); 
+    var rhyInp = tf.tensor2d([[4]]);
+    self.states1A = tf.zeros([1,600]);
+    self.states1B = tf.zeros([1,600]);
+    self.states2A = tf.zeros([1,600]);
+    self.states2B = tf.zeros([1,600]);
+    self.first1A = self.states1A;
+    self.first1B = self.states1B;
+    self.first2A = self.states2A;
+    self.first2B = self.states2B;
+
+    for (let i = 0; i < warmupRounds; i++) {
+        postMessage("AI 巴赫正在热身。现在在第 " + (i+1) + "/" + warmupRounds + " 轮");
+        var exodos = self.modelEmb.predict([midiInp, cpcInp, rhyInp]);
+        var embMidi = exodos[0];
+        var embCpc = exodos[1];
+        var embRhy = exodos[2];
+        var embMidiC = tf.concat([embMidi.slice([0,0,0],[1,1,150]),embMidi.slice([0,1,0],[1,1,150])], 2);
+        var embCpcC = tf.concat([embCpc.slice([0,0,0],[1,1,150]),embCpc.slice([0,1,0],[1,1,150])], 2);
+        var totalInp = tf.concat([embMidiC, embCpcC, embRhy],2);
+        var out = self.modelLstm.predict([totalInp, self.states1A, self.states1B, self.states2A, self.states2B]);
+    }
+    // console.log(self.states1A.dataSync())
+    postMessage("AI 巴赫热身完毕！");
+}
+
+loadModels()
+
+// self.lastAiPrediction = {'aiInpMidi':96, 'aiInpCpc':12};
+
+// self.temperature = 0.9;
+self.counter = 0;
+
+onmessage = function(e) {
+    var data = e.data;
+
+    if (data["reset"]){
+        self.states1A = tf.zeros([1,600]);
+        self.states1B = tf.zeros([1,600]);
+        self.states2A = tf.zeros([1,600]);
+        self.states2B = tf.zeros([1,600]);
+        self.first1A = self.states1A;
+        self.first1B = self.states1B;
+        self.first2A = self.states2A;
+        self.first2B = self.states2B;
+    }
+
+    var midiInp = tf.tensor2d([[data['aiInp'].midiArticInd, data['humanInp'].midiArticInd]]);
+    var cpcInp = tf.tensor2d([[data['aiInp'].cpc, data['humanInp'].cpc]]); 
+    var rhyInp = tf.tensor2d([[data['rhythmInd']]]);
+
+    var exodos = self.modelEmb.predict([midiInp, cpcInp, rhyInp]);
+    var embMidi = exodos[0];
+    var embCpc = exodos[1];
+    var embRhy = exodos[2];
+    var embMidiC = tf.concat([embMidi.slice([0,0,0],[1,1,150]),embMidi.slice([0,1,0],[1,1,150])], 2);
+    var embCpcC = tf.concat([embCpc.slice([0,0,0],[1,1,150]),embCpc.slice([0,1,0],[1,1,150])], 2);
+    var totalInp = tf.concat([embMidiC, embCpcC, embRhy],2);
+
+    var predictTime = performance.now();
+    var out = self.modelLstm.predict([totalInp, self.states1A, self.states1B, self.states2A, self.states2B]);
+    predictTime = performance.now() - predictTime;
+    postMessage({'predictTime': predictTime});
+
+    self.states1A = out[1];
+    self.states1B = out[2];
+    self.states2A = out[3];
+    self.states2B = out[4];
+    
+    var logits = out[0]
+    
+    var logits_temp = logits.div(data["temperature"]);
+    var predictedNote = tf.multinomial(logits_temp, 2);
+
+    var output = {
+        'predictTime': -1,
+        'tick' : data['tick'],
+        'midiArticInd' : predictedNote.dataSync()[0],
+    }
+    
+    self.counter = self.counter + 1
+    postMessage(output);
+}
---
+++ dist/precache-manifest.369c4c1dc5fc16f2d610212d24b054da.js
@@ -0,0 +1,362 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74700fcc4f94019024f05c287dc94eef",
+    "url": "/checkpoints/modelsFinal_Emb/group1-shard1of1.bin"
+  },
+  {
+    "revision": "3774823b9a67cbf81bf5920bcc0430ed",
+    "url": "/checkpoints/modelsFinal_Emb/model.json"
+  },
+  {
+    "revision": "48306a3c3fdd8c4d96748988794c0f56",
+    "url": "/checkpoints/modelsFinal_Emb/model_cleaned.json"
+  },
+  {
+    "revision": "033e74f8c7b314e950a078c23a03a1eb",
+    "url": "/checkpoints/modelsFinal_Lstm/group1-shard1of6.bin"
+  },
+  {
+    "revision": "912605837ce78e5191e5e66cd7608687",
+    "url": "/checkpoints/modelsFinal_Lstm/group1-shard2of6.bin"
+  },
+  {
+    "revision": "9400fb2a719fa1b0f221d1813b2d40e6",
+    "url": "/checkpoints/modelsFinal_Lstm/group1-shard3of6.bin"
+  },
+  {
+    "revision": "6c6c1629741c73fb4bef81887f60b35f",
+    "url": "/checkpoints/modelsFinal_Lstm/group1-shard4of6.bin"
+  },
+  {
+    "revision": "8c63fe47f2d9f204566a9b2e3bc98811",
+    "url": "/checkpoints/modelsFinal_Lstm/group1-shard5of6.bin"
+  },
+  {
+    "revision": "616cfe81ba3a57201a06082ffaed1677",
+    "url": "/checkpoints/modelsFinal_Lstm/group1-shard6of6.bin"
+  },
+  {
+    "revision": "072cbd8c81785aab7f606bed4786b0a8",
+    "url": "/checkpoints/modelsFinal_Lstm/model.json"
+  },
+  {
+    "revision": "96dd7c20349fff584b2e",
+    "url": "/css/app.efbc038c.css"
+  },
+  {
+    "revision": "7c170dfefad4919cb38c",
+    "url": "/css/chunk-vendors.9ea2ccaa.css"
+  },
+  {
+    "revision": "ac129fc41c4e67320d23",
+    "url": "/css/piano.e03394fc.css"
+  },
+  {
+    "revision": "31d28485e1cf7369272270fd730327c0",
+    "url": "/fonts/remixicon.31d28485.eot"
+  },
+  {
+    "revision": "881fbc46361e0c0e5f003c159b2f3005",
+    "url": "/fonts/remixicon.881fbc46.woff"
+  },
+  {
+    "revision": "888e61f04316f10bddfff7bee10c6dd0",
+    "url": "/fonts/remixicon.888e61f0.ttf"
+  },
+  {
+    "revision": "9915fef980fa539085da55b84dfde760",
+    "url": "/fonts/remixicon.9915fef9.woff2"
+  },
+  {
+    "revision": "0dd6d552a3cf5817b10c4bafe6a2a851",
+    "url": "/globalTokenIndexDict.json"
+  },
+  {
+    "revision": "95138f36e015ad912c37db92164f5844",
+    "url": "/img/remixicon.95138f36.svg"
+  },
+  {
+    "revision": "899abc892baeeda41bb41a2f7897a044",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96dd7c20349fff584b2e",
+    "url": "/js/app.fd46be95.js"
+  },
+  {
+    "revision": "7c170dfefad4919cb38c",
+    "url": "/js/chunk-vendors.53aad1dd.js"
+  },
+  {
+    "revision": "ac129fc41c4e67320d23",
+    "url": "/js/piano.88ce1aaa.js"
+  },
+  {
+    "revision": "04791ff7ee8b6f612f9e0a5fc3607247",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "3f008c0d628775e8cb6e83593aaff6e0",
+    "url": "/neuralWorker.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "92242875cc8d992c1ac77ce4e81fe38c",
+    "url": "/samples/harpischord/A2.mp3"
+  },
+  {
+    "revision": "10711f55ec7b2e90d39f32d7cf811009",
+    "url": "/samples/harpischord/A3.mp3"
+  },
+  {
+    "revision": "c1841e3f09934af38477992ea958f692",
+    "url": "/samples/harpischord/A4.mp3"
+  },
+  {
+    "revision": "be331c1759235519e98c22fe8441703a",
+    "url": "/samples/harpischord/A5.mp3"
+  },
+  {
+    "revision": "7446a09dc4a8b3b09e1f06bf4c91fa3b",
+    "url": "/samples/harpischord/A6.mp3"
+  },
+  {
+    "revision": "60dde4c411b7fd3bdb0f49ccba81affd",
+    "url": "/samples/harpischord/As2.mp3"
+  },
+  {
+    "revision": "05e1509628a426efb91ef80b55ab7d20",
+    "url": "/samples/harpischord/As3.mp3"
+  },
+  {
+    "revision": "eccd3a5291e37f6b81a2312a09c46198",
+    "url": "/samples/harpischord/As4.mp3"
+  },
+  {
+    "revision": "130dfc812fec64528abdbe61b03c0109",
+    "url": "/samples/harpischord/As5.mp3"
+  },
+  {
+    "revision": "6ba4cf7252753f923f6c1209bacb93a8",
+    "url": "/samples/harpischord/As6.mp3"
+  },
+  {
+    "revision": "3a45e707c78b29bd3bc25d34f4874157",
+    "url": "/samples/harpischord/B2.mp3"
+  },
+  {
+    "revision": "a6d72049ffa54e69d784ffddb2ce41d7",
+    "url": "/samples/harpischord/B3.mp3"
+  },
+  {
+    "revision": "520faf6463eeae7c80661b38e413bab4",
+    "url": "/samples/harpischord/B4.mp3"
+  },
+  {
+    "revision": "5f053eff929605d8323c03bcaea97f60",
+    "url": "/samples/harpischord/B5.mp3"
+  },
+  {
+    "revision": "ef4ff1d2190364aba7994dbdeb00cbc6",
+    "url": "/samples/harpischord/B6.mp3"
+  },
+  {
+    "revision": "1ac3374d7ea83e45188400056accd418",
+    "url": "/samples/harpischord/C2.mp3"
+  },
+  {
+    "revision": "8afac19cf2823527d61a3d96294da762",
+    "url": "/samples/harpischord/C3.mp3"
+  },
+  {
+    "revision": "2716b590be782898f35dac8fd130b5f3",
+    "url": "/samples/harpischord/C4.mp3"
+  },
+  {
+    "revision": "544a3c25b2082fd2bd6116f4cd90e014",
+    "url": "/samples/harpischord/C5.mp3"
+  },
+  {
+    "revision": "6686ca9fdd5ee1154402a9f066c5c35c",
+    "url": "/samples/harpischord/C6.mp3"
+  },
+  {
+    "revision": "f029c03b02cf74ac79905fba076e7642",
+    "url": "/samples/harpischord/Cs2.mp3"
+  },
+  {
+    "revision": "19771902457e575e2ae60988e7dfd502",
+    "url": "/samples/harpischord/Cs3.mp3"
+  },
+  {
+    "revision": "42b746aec30a9d32c76800fc998cac26",
+    "url": "/samples/harpischord/Cs4.mp3"
+  },
+  {
+    "revision": "6bf365c21745a05a16f0e03571e3b6c0",
+    "url": "/samples/harpischord/Cs5.mp3"
+  },
+  {
+    "revision": "dcf5d7d8d52ee05ac39bdd2ddf827db5",
+    "url": "/samples/harpischord/Cs6.mp3"
+  },
+  {
+    "revision": "819583ee95f20828a1b00e15e07b8709",
+    "url": "/samples/harpischord/D2.mp3"
+  },
+  {
+    "revision": "094485b8f0c0428e1b45ed1ba7a4a54a",
+    "url": "/samples/harpischord/D3.mp3"
+  },
+  {
+    "revision": "9bccd48886f2ebec8534fd6ff00f3b4d",
+    "url": "/samples/harpischord/D4.mp3"
+  },
+  {
+    "revision": "9a9fdc27c6d0ea540dc409e79343e0b8",
+    "url": "/samples/harpischord/D5.mp3"
+  },
+  {
+    "revision": "0942c6d1e2ca13ade6d65da176282ae1",
+    "url": "/samples/harpischord/D6.mp3"
+  },
+  {
+    "revision": "3a132588b1f6915053d72ecd36ba6553",
+    "url": "/samples/harpischord/Ds2.mp3"
+  },
+  {
+    "revision": "c04efcf28d40514a85e1e22457224e17",
+    "url": "/samples/harpischord/Ds3.mp3"
+  },
+  {
+    "revision": "39d616fc66b3e7e757c1f98f9d4597da",
+    "url": "/samples/harpischord/Ds4.mp3"
+  },
+  {
+    "revision": "a2743e2c041ded81304cb0f09865d094",
+    "url": "/samples/harpischord/Ds5.mp3"
+  },
+  {
+    "revision": "5431e9e32f33caf9148e2a8bbcf61068",
+    "url": "/samples/harpischord/Ds6.mp3"
+  },
+  {
+    "revision": "d127586b3b7a2ba399b94499541180e6",
+    "url": "/samples/harpischord/E2.mp3"
+  },
+  {
+    "revision": "ee3b521b575cf5d65fdd38827b30ca3f",
+    "url": "/samples/harpischord/E3.mp3"
+  },
+  {
+    "revision": "b5f01317ebc87693d361613ae4373b98",
+    "url": "/samples/harpischord/E4.mp3"
+  },
+  {
+    "revision": "597d94b328308204c183062163461653",
+    "url": "/samples/harpischord/E5.mp3"
+  },
+  {
+    "revision": "b4aaf804a15aa9ae717702fae16c601a",
+    "url": "/samples/harpischord/E6.mp3"
+  },
+  {
+    "revision": "9f466fd1a42e5b8aaa0ec7f5d12cd3cf",
+    "url": "/samples/harpischord/F2.mp3"
+  },
+  {
+    "revision": "bb5a12c6a22000a21839495d3d15d9ea",
+    "url": "/samples/harpischord/F3.mp3"
+  },
+  {
+    "revision": "f0655305d7c6687ba36028e354376c0e",
+    "url": "/samples/harpischord/F4.mp3"
+  },
+  {
+    "revision": "04c27ba6118482883a8c448d7fc81dac",
+    "url": "/samples/harpischord/F5.mp3"
+  },
+  {
+    "revision": "67625f08a93ff30d7804f94c27f96d12",
+    "url": "/samples/harpischord/F6.mp3"
+  },
+  {
+    "revision": "c8d4b4a6ef39952746f8dea0393e6388",
+    "url": "/samples/harpischord/Fs2.mp3"
+  },
+  {
+    "revision": "41139403ca8f1a2106d6b7099e684001",
+    "url": "/samples/harpischord/Fs3.mp3"
+  },
+  {
+    "revision": "36c1f3a798b4e5c8ae4daf79f61ef2ba",
+    "url": "/samples/harpischord/Fs4.mp3"
+  },
+  {
+    "revision": "83a9bbb97d13f368507783a64dc5d2de",
+    "url": "/samples/harpischord/Fs5.mp3"
+  },
+  {
+    "revision": "64b76db28ad5bd1710d5074ff2f64649",
+    "url": "/samples/harpischord/Fs6.mp3"
+  },
+  {
+    "revision": "4f0b32539739c53f3e220a2187ef699a",
+    "url": "/samples/harpischord/G2.mp3"
+  },
+  {
+    "revision": "015a8d5c2f61507d1bad9749f3793f5f",
+    "url": "/samples/harpischord/G3.mp3"
+  },
+  {
+    "revision": "ea26b39aed505edd9b2391bfd5f9f53a",
+    "url": "/samples/harpischord/G4.mp3"
+  },
+  {
+    "revision": "2f13bde9b0bd9d31b15d25f32c755aef",
+    "url": "/samples/harpischord/G5.mp3"
+  },
+  {
+    "revision": "01bc6b287f329b1d77e593652005eea9",
+    "url": "/samples/harpischord/G6.mp3"
+  },
+  {
+    "revision": "f30b67e739a53478a8d66182cce45213",
+    "url": "/samples/harpischord/Gs2.mp3"
+  },
+  {
+    "revision": "76d5204e0467c1c3ca71e558ee54e163",
+    "url": "/samples/harpischord/Gs3.mp3"
+  },
+  {
+    "revision": "55103ee3accb2f5e06fe7b66f9eb6567",
+    "url": "/samples/harpischord/Gs4.mp3"
+  },
+  {
+    "revision": "c97973c3bd734349c0af07b91daf8fca",
+    "url": "/samples/harpischord/Gs5.mp3"
+  },
+  {
+    "revision": "45fb9aad3470def80a7ad1abb93412b0",
+    "url": "/samples/harpischord/Gs6.mp3"
+  },
+  {
+    "revision": "383fd7c7752164712289e79e4e24cb04",
+    "url": "/samples/metronome/C0.mp3"
+  },
+  {
+    "revision": "40e2656be946a51ba108f700eda08ea9",
+    "url": "/samples/metronome/Cs0.mp3"
+  },
+  {
+    "revision": "dec622ef2759e6a33aa6aaacdf8386c3",
+    "url": "/tf.min.js"
+  },
+  {
+    "revision": "f760a0ffb6f3c337b64bdf11ea2cd426",
+    "url": "/tf.min.js.html"
+  }
+]);
